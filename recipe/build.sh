dd706193f7d21705d59e02a3f46e018ba9419b86
Ray Donnelly <mingw.android@gmail.com>
2017-08-30 13:14:48 +0100

    Ooops

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,7 +21,7 @@ echo "    SRLIBDIR=lib"                                                         
 echo "  fi"                                                                                                                                             >> ${PREFIX}/bin/pkg-config
 echo "  if [[ \${CONDA_BUILD_PKG_CONFIG_LOG} == yes ]]; then"                                                                                           >> ${PREFIX}/bin/pkg-config
 echo "    echo \"Calling: PKG_CONFIG_PATH=\$(\${CC} -print-sysroot)/usr/share/pkgconfig:\$(\${CC} -print-sysroot)/usr/\${SRLIBDIR}/pkgconfig \\"        >> ${PREFIX}/bin/pkg-config
-echo "      \${PREFIX}/bin/pkg-config.bin --define-prefix --debug \"\$@\"" > /tmp/pkg-config-\$$.log"                                                   >> ${PREFIX}/bin/pkg-config
+echo "      \${PREFIX}/bin/pkg-config.bin --define-prefix --debug \"\$@\" > /tmp/pkg-config-\$$.log\""                                                  >> ${PREFIX}/bin/pkg-config
 echo "    PKG_CONFIG_PATH=\$(\${CC} -print-sysroot)/usr/share/pkgconfig:\$(\${CC} -print-sysroot)/usr/\${SRLIBDIR}/pkgconfig \\"                        >> ${PREFIX}/bin/pkg-config
 echo "      \${PREFIX}/bin/pkg-config.bin --define-prefix --debug \"\$@\" >> /tmp/pkg-config-\$$.log 2>&1 || true"                                      >> ${PREFIX}/bin/pkg-config
 echo "  fi"                                                                                                                                             >> ${PREFIX}/bin/pkg-config
