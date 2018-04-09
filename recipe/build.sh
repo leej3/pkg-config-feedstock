95b95fdbca3fd7f1a1dad50c8ce135ff2c19ffe2
Ray Donnelly <mingw.android@gmail.com>
2018-04-09 21:14:45 +0100

    Replace prefixed pkg-config exe with symlink to wrapper script
    Because if it gets run the we do not get the chance to modify necessary env vars (for sysroot .pc lookup etc)

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,4 +12,6 @@ make install
 # conda customization for CDT packages, emitted for other OSes too incase of cross-compilation
 mv ${PREFIX}/bin/pkg-config ${PREFIX}/bin/pkg-config.bin
 cp "${RECIPE_DIR}"/pkg-config ${PREFIX}/bin
-chmod +x ${PREFIX}/bin/pkg-config
+rm -f ${PREFIX}/bin/${HOST}-pkg-config
+ln -s ${PREFIX}/bin/pkg-config ${PREFIX}/bin/${HOST}-pkg-config
+chmod +x ${PREFIX}/bin/pkg-config ${PREFIX}/bin/${HOST}-pkg-config
