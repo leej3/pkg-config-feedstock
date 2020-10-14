81a87b8daeaeacfa5797d7c3e96fc8a1b060ef3e
Isuru Fernando <isuruf@gmail.com>
2020-10-14 11:18:35 -0500

    without internal glib

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ mkdir -p ${PREFIX}/include
 
 ./configure --prefix=${PREFIX}    \
             --host=${HOST}        \
-            --with-internal-glib
+            --without-internal-glib
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
