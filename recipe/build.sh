dd7f9dddf7493765ef33bf2c69e45d8aaf0a2d5f
Isuru Fernando <isuruf@gmail.com>
2020-10-14 13:19:29 -0500

    Fix missing glibconfig.h

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 mkdir -p ${PREFIX}/include
 
-export GLIB_CFLAGS="-I${PREFIX}/include/glib-2.0"
+export GLIB_CFLAGS="-I${PREFIX}/include/glib-2.0 -I${PREFIX}/lib/glib-2.0/include"
 export GLIB_LIBS="${PREFIX}/lib/libglib-2.0.a"
 
 ./configure --prefix=${PREFIX}    \
